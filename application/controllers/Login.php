f5761c2cff5471ab457557db3be7745031fb365f
Unknown <tolentinojohnkenneth007@gmail.com>
2017-11-11 18:08:41 +0800

    Added title to response notification

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -165,7 +165,7 @@ class Login extends CORE_Controller {
                         echo json_encode($response);
 
                     }else{ //not valid
-
+                         $response['title']='Authentication Failed';
                         $response['stat']='error';
                         $response['msg']='Invalid username or password.';
                         echo json_encode($response);
